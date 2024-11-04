afe89f3772fce525af26df10d40d01cd51196511
perronivitor <vitorope48@gmail.com>
2024-11-03 22:02:26 -0300

    [MODULO-03] - configura hilt

--- app/src/main/java/com/example/droidchat/MainActivity.kt
+++ app/src/main/java/com/example/droidchat/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.droidchat.navigation.ChatNavHost
 import com.example.droidchat.ui.ChatApp
 import com.example.droidchat.ui.theme.DroidChatTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
