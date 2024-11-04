ce6ad189714c1c4e8f2f2d9efac9d6f8b60ffe60
perronivitor <vitorope48@gmail.com>
2024-11-03 21:49:37 -0300

    [MODULO-03] - atualiza libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.droidchat"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.droidchat"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
