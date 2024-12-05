4ed7f3342ac4bb480a453b59c06354914b3aa652
perronivitor <vitorope48@gmail.com>
2024-12-05 20:32:32 -0300

    [MODULO-03] - cria classe lidar com dispatcher

---
+++ app/src/main/java/com/example/droidchat/data/di/CoroutinesDispatcherModule.kt
@@ -0,0 +1,34 @@
+package com.example.droidchat.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesDispatcherModule {
+
+    @IoDispatcher
+    @Provides
+    @Singleton
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @DefaultDispatcher
+    @Provides
+    @Singleton
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+}
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
--- app/src/main/java/com/example/droidchat/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/droidchat/data/repository/AuthRepositoryImpl.kt
@@ -1,25 +1,32 @@
 package com.example.droidchat.data.repository
 
+import com.example.droidchat.data.di.IoDispatcher
 import com.example.droidchat.data.network.NetworkDataSource
 import com.example.droidchat.data.network.model.AuthRequest
 import com.example.droidchat.data.network.model.CreateAccountRequest
 import com.example.droidchat.model.CreateAccount
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class AuthRepositoryImpl @Inject constructor(
     private val networkDataSource: NetworkDataSource,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : AuthRepository {
     override suspend fun signUp(createAccount: CreateAccount): Result<Unit> {
-        return runCatching {
-            networkDataSource.signUp(
-                request = CreateAccountRequest(
-                    username = createAccount.username,
-                    password = createAccount.password,
-                    firstName = createAccount.firstName,
-                    lastName = createAccount.lastName,
-                    profilePictureId = createAccount.profilePictureId
+        return withContext(ioDispatcher) {
+            runCatching {
+                networkDataSource.signUp(
+                    request = CreateAccountRequest(
+                        username = createAccount.username,
+                        password = createAccount.password,
+                        firstName = createAccount.firstName,
+                        lastName = createAccount.lastName,
+                        profilePictureId = createAccount.profilePictureId
+                    )
                 )
-            )
+            }
         }
     }
 
