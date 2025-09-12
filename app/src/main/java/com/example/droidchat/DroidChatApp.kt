a56c90ab3da0aa5fccfc76c28fda1bd1da49f374
perronivitor <vitorope48@gmail.com>
2025-09-11 22:36:36 -0300

    [MODULO-05] - Entendendo e criando o canal de notificação

--- app/src/main/java/com/example/droidchat/DroidChatApp.kt
+++ app/src/main/java/com/example/droidchat/DroidChatApp.kt
@@ -1,7 +1,31 @@
 package com.example.droidchat
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DroidChatApp : Application()
+class DroidChatApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            CHAT_MESSAGES_CHANNEL_ID,
+            getString(R.string.notification_chat_messages_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        const val CHAT_MESSAGES_CHANNEL_ID = "chat_messages"
+    }
+}
