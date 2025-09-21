f4abf3b7d4a6e7f5ac585a11dbc4f86f6d3b1647
perronivitor <vitorope48@gmail.com>
2025-09-21 12:41:50 -0300

    [MODULO-05] - Verificando se o aplicativo está em foreground antes de disparar uma notificação

--- app/src/main/java/com/example/droidchat/DroidChatApp.kt
+++ app/src/main/java/com/example/droidchat/DroidChatApp.kt
@@ -4,13 +4,17 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DroidChatApp : Application() {
+class DroidChatApp : Application(), DefaultLifecycleObserver {
 
     override fun onCreate() {
-        super.onCreate()
+        super<Application>.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
         createNotificationChannel()
     }
 
@@ -25,7 +29,19 @@ class DroidChatApp : Application() {
         notificationManager.createNotificationChannel(channel)
     }
 
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        onAppForeground = true
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        onAppForeground = false
+    }
+
     companion object {
         const val CHAT_MESSAGES_CHANNEL_ID = "chat_messages"
+        var onAppForeground: Boolean = false
+            private set
     }
 }
--- app/src/main/java/com/example/droidchat/service/FcmMessagingService.kt
+++ app/src/main/java/com/example/droidchat/service/FcmMessagingService.kt
@@ -51,7 +51,12 @@ class FcmMessagingService : FirebaseMessagingService() {
                 val notificationPayloadJsonString = message.data["messagePayload"]
                 notificationPayloadJsonString?.let { payloadString ->
                     val notificationData = notificationPayloadParse.parse(payloadString)
-                    chatNotificationManager.notifyNewMessage(notificationData)
+
+                    if (DroidChatApp.onAppForeground) {
+                        chatNotificationManager.notifyNewMessage(notificationData)
+                        return@launch
+                    }
+
                     sendNotification(notificationData)
                 }
             }
